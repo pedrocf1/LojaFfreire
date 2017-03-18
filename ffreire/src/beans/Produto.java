092489c857dcdf4876f28644d356063da87e3253
pedrofc1 <pedrofc31.31@gmail.com>
2017-03-18 00:08:10 -0300

    Produto
    adição da beans Produto

---
+++ ffreire/src/beans/Produto.java
@@ -0,0 +1,13 @@
+package beans;
+
+public class Produto {
+	
+	private int tamanho;
+	private double preco;
+	private String descrição;
+	static private int codigo;
+	private int subCodigo;
+	private int quantidade;
+	private String imagem;
+
+}
---
+++ ffreire/src/repositorio/IProduto.java
@@ -0,0 +1,7 @@
+package repositorio;
+
+public interface IProduto {
+	
+	
+
+}
